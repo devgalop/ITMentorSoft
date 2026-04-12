c8f2e269f68826a5e376895d5d4165e0fa6f106f
[devgalop] <devgalop@gmail.com>
2026-04-12 06:39:35 -0500

    feat: Complementa los escenarios de prueba de login handler
    Se complementan los escenarios de prueba de LoginHandler ya que solo tenía prueba del escenario feliz

--- tests/features/user_management/login/test_login_handler.py
+++ tests/features/user_management/login/test_login_handler.py
@@ -36,3 +36,53 @@ async def test_when_credentials_are_valid_should_login_user():
     user_repository.get_user_by_email.assert_called_once_with("test@example.com")
     password_hasher.verify_password.assert_called_once_with(sample_pass, "hashed_password")
     token_generator.generate_token.assert_called_once()
+    
+@pytest.mark.asyncio
+async def test_when_email_does_not_exist_should_return_empty_response():
+    user_repository = AsyncMock()
+    password_hasher = Mock()
+    token_generator = Mock()
+    
+    user_repository.get_user_by_email = AsyncMock(return_value=None)
+    
+    handler = LoginHandler(user_repository, password_hasher, token_generator)
+    response = await handler.handle(LoginRequest(
+        email="invalid@example.com",
+        password="Password123!"
+    ))
+    
+    assert response.is_successful == False
+    assert response.token == ""
+    assert response.expiration_time == 0
+    user_repository.get_user_by_email.assert_called_once_with("invalid@example.com")
+    password_hasher.verify_password.assert_not_called()
+    token_generator.generate_token.assert_not_called()
+    
+@pytest.mark.asyncio
+async def test_when_password_is_incorrect_should_return_empty_response():
+    user_repository = AsyncMock()
+    password_hasher = Mock()
+    token_generator = Mock()
+    
+    user_repository.get_user_by_email = AsyncMock(return_value=User(
+        username="testuser",
+        email="test@example.com",
+        password_hashed="hashed_password",
+        status=UserStatus.ACTIVE,
+        role = UserRole.STUDENT
+    ))
+    password_hasher.verify_password = Mock(return_value=False)
+    
+    handler = LoginHandler(user_repository, password_hasher, token_generator)
+    response = await handler.handle(LoginRequest(
+        email="test@example.com",
+        password="IncorrectPassword12!"
+    ))
+    
+    assert response.is_successful == False
+    assert response.token == ""
+    assert response.expiration_time == 0
+    user_repository.get_user_by_email.assert_called_once_with("test@example.com")
+    password_hasher.verify_password.assert_called_once_with("IncorrectPassword12!", "hashed_password")
+    token_generator.generate_token.assert_not_called()
+    
